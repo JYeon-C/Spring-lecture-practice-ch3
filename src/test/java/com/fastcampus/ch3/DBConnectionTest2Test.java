a6c5effd2b985cec7b069243e38c0241cfac4850
JungYeon <72572045+Jungyeon-Choi-hubbb@users.noreply.github.com>
2023-03-08 23:05:18 +0900

    23.02.23 ch3.11-13

---
+++ src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
@@ -0,0 +1,32 @@
+package com.fastcampus.ch3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DBConnectionTest2Test {
+    @Autowired // 아래 주석 처리문과 같은 역할. AC 주입받아 사용 가능
+    DataSource ds;
+
+    @Test
+    public void springJdbcConnectTest() throws Exception{
+//        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+//        DataSource ds = ac.getBean(DataSource.class);
+
+        Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
+
+        System.out.println("conn = " + conn);
+        assertTrue(conn!=null); // 테스트가 성공했는지 assert문으로 확인한다. 괄호 안에 조건식이 true면 테스트 성공, 아니면 실패
+    }
+}
